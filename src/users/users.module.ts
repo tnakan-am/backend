6801b54a8655f343c7480996df0a885f2936f14c
Vahe_Gevorgyan1 <vahe_gevorgyan1@epam.com>
2025-08-28 17:22:12 +0400

    Update .gitignore and enhance UserModule with AuthGuard
    - Added CLAUDE.md to .gitignore to prevent tracking of the file.
    - Integrated AuthGuard into UserModule as a global guard for enhanced authentication.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,18 @@ import { UserService } from './users.service';
 import { UserController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './entities/user.entity';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Users])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    }
+  ],
   exports: [UserService],
 })
 export class UserModule {}
