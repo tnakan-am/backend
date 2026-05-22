9979daf7b20a4878f2e5b47d511a0b6f7636e5bb
Vahe_Gevorgyan1 <vahe_gevorgyan1@epam.com>
2026-05-22 21:33:28 +0400

    Update dependencies in package-lock.json and enhance application health check endpoint
    - Updated various Babel dependencies to their latest versions for improved performance and security.
    - Added a new public decorator to the AppController to allow unauthenticated access to the health check endpoint.
    - Modified Jest configuration to simplify the transformation regex for TypeScript files.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './auth/public.decorator';
 
 @Controller()
 export class AppController {
@@ -10,6 +11,7 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  @Public()
   @Get('health')
   health(): { status: 'ok' } {
     return { status: 'ok' };
